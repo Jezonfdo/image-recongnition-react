a4e06eddd744090918b383c584feb23de46e4590
Jezonfdo <jezonfernando.creator@gmail.com>
2021-01-12 17:20:52 +0530

    version 0.1

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navigation.css';
+import 'tachyons'
+
+function Navigation() {
+  return (
+    <div className="Navigation">
+    	<nav>
+    		<span id="sign-out">Sign Out</span>
+    	</nav>
+    </div>
+  );
+}
+
+export default Navigation;
